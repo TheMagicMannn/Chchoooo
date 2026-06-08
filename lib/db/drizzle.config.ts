90a99c39a643a8ddf375fa63914206903723fb32
TheMagicMannn <211097201+TheMagicMannn@users.noreply.github.com>
2026-06-07 23:17:25 -0400

    Change DATABASE_URL to NEON_DATABASE_URL check

--- lib/db/drizzle.config.ts
+++ lib/db/drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 import path from "path";
 
-if (!process.env.DATABASE_URL) {
+if (!process.env.NEON_DATABASE_URL) {
   throw new Error("DATABASE_URL, ensure the database is provisioned");
 }
 
